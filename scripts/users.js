9f155971c91e484f37a1ae2c67d1ab12e2161bc0
Jaerker <johanjakberger@gmail.com>
2024-01-30 13:05:36 +0100

    Add template users in users.js

--- scripts/users.js
+++ scripts/users.js
@@ -1,5 +1,19 @@
 /* Här skapar ni en array bestående av minst 3 st användarobjekt */
 
 let users = [
-    
+    {
+        id:1,
+        username: 'jaerker',
+        password: 'sverker'
+    },
+    {
+        id:2,
+        username: 'perarne',
+        password: 'iheartvolvo'
+    },
+    {
+        id:3,
+        username: 'carita',
+        password: 'husvagnsliv'
+    }
 ];

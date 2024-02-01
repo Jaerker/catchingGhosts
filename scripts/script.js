00a53062b1175a5ddf4b1fa1a88a85f8b7f96936
Jaerker <johanjakberger@gmail.com>
2024-02-01 12:27:34 +0100

    configured files

--- scripts/script.js
+++ scripts/script.js
@@ -1,66 +1,116 @@
 'use strict';
 
 window.addEventListener('load', () => {
-    //Här kickar ni igång ert program
+  //Här kickar ni igång ert program
+  document.querySelector('#create').addEventListener('click', registerUser);
 });
 
 /**
  * Hämtar hem alla användare i vår localStorage lista, alternativt skapar och populerar den med det som finns i users.js
- * 
+ *
  * @returns {[...{username: string, password: string}]} Returnerar en array med objekt innehållande {username, password}
  */
 function getUsers() {
+  //Om users inte existerar i localStorage => lägg till de users vi har förberett till localStorage. (Du kan alltså ta bort alla användare och lägga
+  // in nya utan att den populeras igen. Det är bara när man tar bort den helt som dessa läggs i automatiskt)
+  if (!localStorage.getItem('users')) {
+    localStorage.setItem('users', JSON.stringify(users));
+  }
 
-    //Om users inte existerar i localStorage => lägg till de users vi har förberett till localStorage. (Du kan alltså ta bort alla användare och lägga 
-    // in nya utan att den populeras igen. Det är bara när man tar bort den helt som dessa läggs i automatiskt)
-    if(!localStorage.getItem('users')){
-        localStorage.setItem('users', JSON.stringify(users));
-    }
-
-    //Skickar tillbaka localStorage listan som nu är populerad med användare
-    return JSON.parse(localStorage.getItem('users'));
+  //Skickar tillbaka localStorage listan som nu är populerad med användare
+  return JSON.parse(localStorage.getItem('users'));
 }
 
 /**
- * Lägger in användare i användarlistan. 
- * 
+ * Lägger in användare i användarlistan.
+ *
  * @param {string} username Användarnamnet för personen som läggs till i användarlistan
  * @param {string} password Lösenordet för personen som läggs till i användarlistan
- * @returns {void} returnerar inget. 
+ * @returns {void} returnerar inget.
  */
-function addUser(username, password){
-    /*
-     * vi kör getUsers för att hämta listan med användare och lägger dessa i en ny array, där vi även lägger till den nya användaren (därav de 
-     * tre punkterna på funktions-namnet som betyder att vi vill ha alla värden från returvärdet, samt något mer) och sätter users i localStorage 
-     * efter den nya arrayen som det bildar!
-     * 
-     * ID får vi fram genom att kolla om listan har någon på sista plats (alltså om den är tom eller inte) och sätter id numret efter antingen sista 
-     * IDt +1, eller 1 för en tom array.
-     * 
-     */
-    localStorage.setItem('users', JSON.stringify([...getUsers(), {username: username, password:password, id:getUsers()[getUsers().length-1]?.id+1 || 1}]));
+function addUser(username, password) {
+  /*
+   * vi kör getUsers för att hämta listan med användare och lägger dessa i en ny array, där vi även lägger till den nya användaren (därav de
+   * tre punkterna på funktions-namnet som betyder att vi vill ha alla värden från returvärdet, samt något mer) och sätter users i localStorage
+   * efter den nya arrayen som det bildar!
+   *
+   * ID får vi fram genom att kolla om listan har någon på sista plats (alltså om den är tom eller inte) och sätter id numret efter antingen sista
+   * IDt +1, eller 1 för en tom array.
+   *
+   */
+  localStorage.setItem(
+    'users',
+    JSON.stringify([
+      ...getUsers(),
+      {
+        username: username,
+        password: password,
+        id: getUsers()[getUsers().length - 1]?.id + 1 || 1,
+      },
+    ])
+  );
 }
 
 /**
  * Tar bort en användare från listan. Finns inte användaren så händer inget (alltså om fel ID har kommit in som parameter)
- * 
+ *
  * @param {number} userId Användarens ID nummer
- * 
+ *
  * @returns {void} returnerar inget
  */
-function removeUser(userId){
-    localStorage.setItem('users', JSON.stringify(getUsers().filter(user => user.id !== userId)));
+function removeUser(userId) {
+  localStorage.setItem(
+    'users',
+    JSON.stringify(getUsers().filter((user) => user.id !== userId))
+  );
 }
 
 /**
- * Sätter användaren för spelrundan. Kan vara bra att köra denna funktion när man lyckats logga in eller registrera en ny 
+ * Sätter användaren för spelrundan. Kan vara bra att köra denna funktion när man lyckats logga in eller registrera en ny
  * användare, och bara passera in själva ID:t om man har det tillgängligt, underlättar väldigt om vi ska använda oss av att ta bort användare.
- * 
- * 
+ *
+ *
  * @param {number} userId Användarens ID nummer
- * 
+ *
  * @return {void} returnerar inget
  */
-function setCurrentUser(userId){
-    oGameData.currentUser = getUsers().filter(user => user.id === userId)[0] || {};
+function setCurrentUser(userId) {
+  oGameData.currentUser =
+    getUsers().filter((user) => user.id === userId)[0] || {};
+}
+
+function registerUser(event) {
+  event.preventDefault();
+  const message = document.querySelector('#registrationMsg'); 
+  const username = document.querySelector('#registerUsername').value;
+  const password = document.querySelector('#registerPassword').value;
+  const passwordAgain = document.querySelector('#registerPasswordAgain').value;
+
+  if (username.length === 0 || password.length === 0 || passwordAgain.length === 0){
+    message.textContent = 'Du måste skriva något i rutorna!';
+  }
+  else{
+
+    if (getUsers().some((user) => user.name === username)) {
+        message.textContent = 'Användaren finns redan!';
+      } else if (password !== passwordAgain) {
+        message.textContent = 'Lösenordet stämmer inte överens!';
+      } else {
+        addUser(username, password);
+        document.querySelector('#loginMsg').textContent = 'Användare skapad!';
+      }
+
+  }
+    
+
+
+
+}
+
+function toggleForms() {
+    //formLoginDiv till formRegistrationDiv
+
+    //formRegistrationDiv till formLoginDiv
+
+
 }
